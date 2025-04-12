095a4fee4a7a9c04110b8f50e4f983e5d1c1d5c0
Rayid_ashraf <rayidashrafdar@gmail.com>
2025-04-12 15:26:29 +0530

    fix: secure supabase url and key

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://aakklcsevurowwcfydnn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFha2tsY3NldnVyb3d3Y2Z5ZG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3MjYyMzcsImV4cCI6MjA1ODMwMjIzN30.IodyFm2XI8eDIOWWC410PLzbQO8jK6oqnTI-hiFNpF8";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
